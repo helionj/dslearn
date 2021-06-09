b1c95f715cf372a5c11d80f91321ddf12908a65d
helionporto <helionporto@hotmail.com>
2021-06-08 21:20:11 -0400

    Deliver and seed

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/enums/DeliverStatus.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dslearnbds.entities.enums;
+
+public enum DeliverStatus {
+	PENDING(1),
+	ACCEPTED(2),
+	REJECTED(3);
+	
+	
+	private int code;
+	
+	private DeliverStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public static DeliverStatus valueOf(int code) {
+		for(DeliverStatus value: DeliverStatus.values()) {
+			if(code == value.getCode()) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid argument ResourceType code");
+	}
+
+}
